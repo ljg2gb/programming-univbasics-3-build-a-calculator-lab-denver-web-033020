e32206ebc64f35e0b1a06a6b84b2e6632d72e502
Lydia Gregory <ljg2gb@virginia.edu>
2020-03-10 23:17:12 +0000

    Automatically backed up by Learn

--- lib/math.rb
+++ lib/math.rb
@@ -1,23 +1,35 @@
 def addition(num1, num2)
-
+  puts num1 + num2
 end
+
+addition (1, 2)
 
 def subtraction(num1, num2)
-
+  puts num1 - num2
 end
+
+subtraction (1, 2)
 
 def division(num1, num2)
-
+  puts num1 / num2
 end
+
+division (1, 2)
 
 def multiplication(num1, num2)
-
+  puts num1 * num2
 end
+
+multiplication (1, 2)
 
 def modulo(num1, num2)
-
+  puts num1 % num2
 end
+
+modulo (1, 3)
 
 def square_root(num)
-
+  puts Math.sqrt(num)
 end
+
+square_root (81)
